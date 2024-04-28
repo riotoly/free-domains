175a8a575fa7b863adf925785bd4742275b2f824
riotly <harshskb73@gmail.com>
2024-04-28 07:01:33 +0530

    uptime.is-an.app.js

---
+++ domains/uptime.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Subdomain for a project',
+  domain: 'is-an.app',
+  subdomain: 'uptime',
+  owner: {
+    email: 'domainoh011@gmail.com',
+  },
+  record: {
+    CNAME: 'uptime-kuma-doyc.onrender.com'
+  },
+  proxy: false
+})
